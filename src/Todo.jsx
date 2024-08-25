596665cef005659697412a09347da62d0451a251
Smart-imran <rst.imran4@gmail.com>
2024-08-26 01:47:44 +0600

    conditional rendering option 2

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -8,16 +8,31 @@
 
 
 // conditional  rendering option 1
+// export default function Todo({task,isDone}){
+//     if(isDone === true){
+//         return <li>
+//             Finised : {task}
+//         </li>
+//     }
+
+//     else{
+//         return <li>
+//             Do again: {task}
+//         </li>
+//     }
+// }
+
+// conditional rendering option 2
+
 export default function Todo({task,isDone}){
-    if(isDone === true){
+    if(isDone){
         return <li>
             Finised : {task}
-        </li>
+         </li>
     }
 
-    else{
-        return <li>
-            Do again: {task}
+    return <li>
+            Do Again: {task}
         </li>
-    }
+
 }
